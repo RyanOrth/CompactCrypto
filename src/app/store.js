84440db6988e65f6f9fa77cf55526b1ed37488ae
Ethan Rasgorshek <erasgorshek2@huskers.unl.edu>
2021-10-22 15:28:00 -0500

    Removed graphSlice from store.js.

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import graphSlice from '../features/graph/graphSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    [graphSlice.name]: graphSlice.reducer,
   },
 });
