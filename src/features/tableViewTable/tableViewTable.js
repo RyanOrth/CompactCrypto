f716641858b67e97de044e416342ed092f6fbd15
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2021-10-29 18:27:50 -0500

    Pushing for end of day

--- src/features/tableViewTable/tableViewTable.js
+++ src/features/tableViewTable/tableViewTable.js
@@ -7,100 +7,100 @@ import jsonData from "../../data/data.json";
 import { COLUMNS } from "./tableViewTableColumns";
 import { getFavoriteList, setFavoriteList } from "./tableViewTableSlice";
 import { selectValue } from "../dropDown/dropDownSlice";
-import './tableViewTableCSS.css';
+// import './tableViewTableCSS.css';
 
 
 
 export const TableViewTable = () => {
 
-	const favoriteList = useSelector(getFavoriteList);
-	const filterType = useSelector(selectValue);
-	const dispatch = useDispatch();
+  const favoriteList = useSelector(getFavoriteList);
+  const filterType = useSelector(selectValue);
+  const dispatch = useDispatch();
 
-	const updateFavoriteList = (favoriteList) =>
-		dispatch(
-			setFavoriteList({
-				favoriteList,
-			})
-		)
+  const updateFavoriteList = (favoriteList) =>
+    dispatch(
+      setFavoriteList({
+        favoriteList,
+      })
+    )
 
-	// const addFavorite = (favorite) =>
-	//   dispatch(
-	//     pushFavorite({
-	//       favorite,
-	//     })
-	//   );
-	// const deleteFavorite = (favorite) =>
-	//   dispatch(
-	//     popFavorite({
-	//       favorite,
-	//     })
-	//   );
+  // const addFavorite = (favorite) =>
+  //   dispatch(
+  //     pushFavorite({
+  //       favorite,
+  //     })
+  //   );
+  // const deleteFavorite = (favorite) =>
+  //   dispatch(
+  //     popFavorite({
+  //       favorite,
+  //     })
+  //   );
 
-	const columns = useMemo(() => COLUMNS, []);
-	const data = useMemo(() => jsonData, []);
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => jsonData, []);
 
-	const tableInstance = useTable({
-		columns,
-		data
-	}, useSortBy);
+  const tableInstance = useTable({
+    columns,
+    data
+  }, useSortBy);
 
-	const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
 
-	return (
-		<table {...getTableProps()}>
-			<thead>
-				{
-					headerGroups.map(headerGroup => (
-						<tr {...headerGroup.getHeaderGroupProps()}>
-							{
-								headerGroup.headers.map(column => (
-									<th {...column.getHeaderProps(column.getSortByToggleProps())}>
-										{column.render('Header')}
-										{
-											(column.Header !== 'Favorite') ?
-												<span style={{ float: 'right' }}>
-													{
-														column.isSorted ? (column.isSortedDesc ?
-															<TiArrowSortedDown size={20} color={'black'} /> :
-															<TiArrowSortedUp size={20} color={'black'} />
-														) : <TiArrowUnsorted size={20} color={'black'} />
-													}
-												</span> : ''
-										}
-									</th>
-								))
-							}
-						</tr>
-					))
-				}
-			</thead>
-			<tbody {...getTableBodyProps()}>
-				{
-					rows.map(row => {
-						prepareRow(row)
-						return (// Here change color if clicked and runs onclick
-							(filterType !== 'Favorites' || favoriteList.includes(row.values.SYMBOL) || favoriteList.length === 0) ?
-								<tr {...row.getRowProps()}>
-									{
-										row.cells.map((cell) => {// Here is changing cell color based on value
-											return <td {...cell.getCellProps()} style={{
-												background: cell.column.Header === 'Gain/Loss' ? cell.value > 0 ? `rgb(50, 125, 0)` : `rgb(150,40,40)` : null,
-											}}>
-												{(cell.column.Header !== 'Favorite') ?
-													cell.render('Cell') :
-													(favoriteList.includes(cell.row.values['SYMBOL'])) ?
-														<IoStar size={30} color={'red'} onClick={() => updateFavoriteList([...favoriteList].filter(n => n !== cell.row.values['SYMBOL']))} /> :
-														<IoStarOutline size={30} color={'black'} onClick={() => updateFavoriteList([...favoriteList, cell.row.values['SYMBOL']])} />}
-											</td>
-										})
-									}
-								</tr>
-								: ''
-						)
-					})
-				}
-			</tbody>
-		</table>
-	)
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {
+          headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {
+                headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render('Header')}
+                    {
+                      (column.Header !== 'Favorite') ?
+                        <span style={{ float: 'right' }}>
+                          {
+                            column.isSorted ? (column.isSortedDesc ?
+                              <TiArrowSortedDown size={20} color={'black'} /> :
+                              <TiArrowSortedUp size={20} color={'black'} />
+                            ) : <TiArrowUnsorted size={20} color={'black'} />
+                          }
+                        </span> : ''
+                    }
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {
+          rows.map(row => {
+            prepareRow(row)
+            return (// Here change color if clicked and runs onclick
+              (filterType !== 'Favorites' || favoriteList.includes(row.values.SYMBOL) || favoriteList.length === 0) ?
+                <tr {...row.getRowProps()}>
+                  {
+                    row.cells.map((cell) => {// Here is changing cell color based on value
+                      return <td {...cell.getCellProps()} style={{
+                        background: cell.column.Header === 'Gain/Loss' ? cell.value > 0 ? `rgb(50, 125, 0)` : `rgb(150,40,40)` : null,
+                      }}>
+                        {(cell.column.Header !== 'Favorite') ?
+                          cell.render('Cell') :
+                          (favoriteList.includes(cell.row.values['SYMBOL'])) ?
+                            <IoStar size={30} color={'red'} onClick={() => updateFavoriteList([...favoriteList].filter(n => n !== cell.row.values['SYMBOL']))} /> :
+                            <IoStarOutline size={30} color={'black'} onClick={() => updateFavoriteList([...favoriteList, cell.row.values['SYMBOL']])} />}
+                      </td>
+                    })
+                  }
+                </tr>
+                : ''
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import { COMPACT_VIEW } from '../constants/action-types';
 import { CandlestickChart } from '../features/graph/CandlestickChart';
 import { NavBar } from '../features/navBar/NavBar';
 import {
-    selectCurrentPage, updateCurrentPage,
-    updateDisplayFilterOptions,
-    updateDisplaySearchBar
+  selectCurrentPage, updateCurrentPage,
+  updateDisplayFilterOptions,
+  updateDisplaySearchBar
 } from '../features/navBar/navBarSlice';
 import { SideTable } from '../features/sideTable/SideTable';
 import './screenCSS/compactView.css';
@@ -14,47 +14,47 @@ import './screenCSS/compactView.css';
 
 
 const CompactView = () => {
-    var currentPage = useSelector(selectCurrentPage);
-    const dispatch = useDispatch();
+  var currentPage = useSelector(selectCurrentPage);
+  const dispatch = useDispatch();
 
-    dispatch(updateCurrentPage(COMPACT_VIEW));
-    dispatch(updateDisplayFilterOptions(false));
-    dispatch(updateDisplaySearchBar(false));
+  dispatch(updateCurrentPage(COMPACT_VIEW));
+  dispatch(updateDisplayFilterOptions(false));
+  dispatch(updateDisplaySearchBar(false));
 
-    return (
-        <div>
-            <NavBar currentPage={COMPACT_VIEW} displayFilterOptions={false} displayGraphOptions={false} displaySearchBar={false} />
-            <div>
-                <div style={{
-                    display: 'none',
-                    justifyContent: 'Right',
-                    alignItems: 'Right',
-                    height: '10vh'
-                }}
-                >
-                    <h1>Compact View</h1>
-                </div>
-                <div style={{
-                    height: '900px',
-                }}>
-                    <div id={'tbbl'} style={{
-                        float: 'left',
-                    }}>
-                        <SideTable />
-                    </div>
-                    <div style={{
-                        float: 'right',
-                        marginRight: '5%',
-                        marginTop: '5%',
-                        width: '55%',
-                        height: '80%',
-                    }}>
-                        <CandlestickChart width={'100%'} height={'100%'} currentPage={'COMPACT_VIEW'} />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <NavBar currentPage={COMPACT_VIEW} displayFilterOptions={false} displayGraphOptions={false} displaySearchBar={false} />
+      <div>
+        <div style={{
+          display: 'none',
+          justifyContent: 'Right',
+          alignItems: 'Right',
+          height: '10vh'
+        }}
+        >
+          <h1>Compact View</h1>
         </div>
-    );
+        <div style={{
+          height: '900px',
+        }}>
+          <div id={'tbbl'} style={{
+            float: 'left',
+          }}>
+            <SideTable />
+          </div>
+          <div style={{
+            float: 'right',
+            marginRight: '5%',
+            marginTop: '5%',
+            width: '55%',
+            height: '80%',
+          }}>
+            <CandlestickChart width={'100%'} height={'100%'} currentPage={'COMPACT_VIEW'} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CompactView;
