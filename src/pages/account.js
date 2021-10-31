7137eade84c568121c86a16129c449cd3e204a4b
RyanOrth <ryan.orth@live.com>
2021-10-31 16:55:40 -0500

    Added notification panel, checkbox still needed

--- src/pages/account.js
+++ src/pages/account.js
@@ -16,6 +16,7 @@ import './screenCSS/account.css';
 
 import { getFavoriteList, setFavoriteList } from "../features/tableViewTable/tableViewTableSlice";
 import { IoStar } from 'react-icons/io5';
+import { FaSquare, FaCheckSquare } from 'react-icons/fa';
 
 const Account = () => {
   var currentPage = useSelector(selectCurrentPage);
@@ -40,27 +41,41 @@ const Account = () => {
       <NavBar currentPage={ACCOUNT_VIEW} displayFilterOptions={false} displayGraphOptions={false} displaySearchBar={false} />
       <div className={'page'}>
         <h1>Account</h1>
-        <div className={'panel'}>
-          <div>
-            <label>Email Notifications</label>
+        <div className={'panels'}>
+          <div className={'panel'}>
+            <div>
+              <label>Email Notifications</label>
 
+            </div>
+            <div>
+              <label for='fEmail'>Email Address</label>
+              <input></input>
+            </div>
           </div>
-          <div>
-            <label for='fEmail'>Email Address</label>
-            <input></input>
-          </div>
-        </div>
 
-        <div className={'panel'}>
-          <div>
-            <label>Favorites</label>
+          <div className={'panel'}>
+            <div>
+              <label>Favorites</label>
 
+            </div>
+            <div>
+              {(currentFavoriteList.length > 0) ?
+                currentFavoriteList.map((favorite) => <div className={'favorite'}> <IoStar onClick={() => updateFavoriteList(currentFavoriteList.filter(item => item !== favorite))}></IoStar> {favorite} </div>)
+                : (<div>No current favorites</div>)}
+            </div>
           </div>
-          <div>
-            {(currentFavoriteList.length > 0) ?
-              currentFavoriteList.map((favorite) => <div className={'favorite'}> <IoStar onClick={() => updateFavoriteList(currentFavoriteList.filter(item => item !== favorite))}></IoStar> {favorite} </div>)
-              : (<div>No current favorites</div>)}
+
+          <div className={'panel'}>
+            <dive>
+              <label>Types of Cryptocurrencies for Notifications</label>
+            </dive>
+            <div className={'notification-list'}>
+              <div className={'notification-item'}><FaSquare />Changes to Favorites</div>
+              <div className={'notification-item'}><FaSquare />Quick Decrease</div>
+              <div className={'notification-item'}><FaSquare />Quick Increase</div>
+            </div>
           </div>
+
         </div>
       </div>
     </div>
