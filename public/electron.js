717ba135ad1568847c572a8a9012e57fdef2b144
RyanOrth <ryan.orth@live.com>
2021-11-18 01:22:50 -0600

    Updated logo for electron builds, multi-os builds

--- public/electron.js
+++ public/electron.js
@@ -19,7 +19,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1024,
     height: 1024,
-    title: "Compact Crypto"
+    title: "Compact Crypto",
+    icon: 'src/icon.ico'
   });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', function () {
