dac884d9bd1899a15389edc7df05fc294e7d67fc
RyanOrth <ryan.orth@live.com>
2021-12-21 22:07:54 -0600

    Updates

---
+++ electron.js
@@ -0,0 +1,32 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow = null;
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 1024,
+    title: "Compact Crypto",
+    icon: 'src/icon.ico'
+  });
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+  mainWindow.on('page-title-updated', function (e) {
+    e.preventDefault()
+  });
+}
